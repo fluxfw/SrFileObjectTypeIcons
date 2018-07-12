9840696275a36907efce38474bf4b40455e0fd8e
Florian <fw@studer-raimann.ch>
2018-07-12 15:38:21 +0200

    Plugin version 5.3

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = 'mimetypeicons';
 $version = '0.0.2';
-$ilias_min_version = '5.1';
-$ilias_max_version = '5.1.99';
+$ilias_min_version = '5.3.0';
+$ilias_max_version = '5.3.999';
 $responsible = 'Fabian Schmid';
 $responsible_mail = 'support@studer-raimann.ch';
