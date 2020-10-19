7a5c11aca5b5ba77280b673afa5e13e0c55e0f06
sragfw <fw@studer-raimann.ch>
2020-10-19 07:11:48 +0200

    ILIAS 5.4

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = 'mimetypeicons';
 $version = '0.0.2';
-$ilias_min_version = '5.3.0';
-$ilias_max_version = '5.3.999';
+$ilias_min_version = '5.4.0';
+$ilias_max_version = '5.4.999';
 $responsible = 'Fabian Schmid';
 $responsible_mail = 'support@studer-raimann.ch';
